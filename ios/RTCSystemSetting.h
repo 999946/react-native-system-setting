f83d84f3f93662b14b8206315aa5a57998b4c29d
lzw <elvis.lu@yolipai.com>
2017-06-14 21:36:32 +0800

    super RN version 0.40 -

--- ios/RTCSystemSetting.h
+++ ios/RTCSystemSetting.h
@@ -7,8 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#else
+#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
+#endif
 
 @interface RCTSystemSetting : RCTEventEmitter <RCTBridgeModule>
 
